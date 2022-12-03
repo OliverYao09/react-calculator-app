d4159d431e6b5d8c5d7bae8d8d00556402d0a405
oliveryao09 <695353020@qq.com>
2022-12-03 14:43:10 +1300

    add digit number
    Allows users to add digit in the calculator

---
+++ src/DigitButton.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { ACTIONS } from './App';
+
+export function DigitButton({ dispatch, digit }) {
+  return (
+    <button
+      onClick={() => dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit } })}
+    >
+      {digit}
+    </button>
+  );
+}
+
+export default DigitButton;
